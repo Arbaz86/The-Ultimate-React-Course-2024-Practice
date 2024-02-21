3b29ae8815453abc34e106aa66b9bd0e864391b6
Arbaz Hussain <96072331+Arbaz86@users.noreply.github.com>
2024-02-21 18:38:03 +0530

    adding live practice code of travel-list project

---
+++ 05-travel-list/live-practice/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Stats from "./components/Stats";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Logo from "./components/Logo";
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="App">
+      <Logo />
+      <Form onAddItems={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+export default App;
