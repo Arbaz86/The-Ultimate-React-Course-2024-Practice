a2383d351b61550c370c4e3108fb9482ed8162a8
Arbaz Hussain <96072331+Arbaz86@users.noreply.github.com>
2024-02-23 17:44:00 +0530

    Simplify total payment calculation
    Removed redundant 'bill' to number conversion, now directly using 'bill + tip'.

--- 05-travel-list/challenges/tip-calculator-v1/src/components/Output.jsx
+++ 05-travel-list/challenges/tip-calculator-v1/src/components/Output.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Output = ({ bill, tip }) => {
   return (
     <h2>
-      You pay ${Number(bill) + tip} (${bill} + ${tip} tip)
+      You pay ${bill + tip} (${bill} + ${tip} tip)
     </h2>
   );
 };
