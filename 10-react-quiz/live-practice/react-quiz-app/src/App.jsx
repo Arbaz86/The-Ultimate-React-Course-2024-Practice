0f06d5b12781455f9f59fc527d82b256beaad656
Arbaz Hussain <arbazh777@gmail.com>
2024-06-24 15:26:32 +0530

    initial commit

---
+++ 10-react-quiz/live-practice/react-quiz-app/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import DateCounter from "./components/DateCounter.jsx";
+
+function App() {
+  return (
+    <div className="app">
+      <DateCounter />
+    </div>
+  );
+}
+
+export default App;
