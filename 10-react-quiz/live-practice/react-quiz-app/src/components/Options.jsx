4c324ce6d6134e1c2b1bf0c3fa98e141da92ae94
Arbaz Hussain <arbazh777@gmail.com>
2024-06-24 17:33:06 +0530

    implement data select and points calculate for Handling New Answers

--- 10-react-quiz/live-practice/react-quiz-app/src/components/Options.jsx
+++ 10-react-quiz/live-practice/react-quiz-app/src/components/Options.jsx
@@ -1,10 +1,23 @@
 import PropTypes from "prop-types";
 
-const Options = ({ question }) => {
+const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button key={option} className="btn btn-option">
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          disabled={hasAnswered}
+        >
           {option}
         </button>
       ))}
@@ -14,6 +27,8 @@ const Options = ({ question }) => {
 
 Options.propTypes = {
   question: PropTypes.object.isRequired,
+  dispatch: PropTypes.any.isRequired,
+  answer: PropTypes.number,
 };
 
 export default Options;
--- 10-react-quiz/live-practice/react-quiz-app/src/components/Question.jsx
+++ 10-react-quiz/live-practice/react-quiz-app/src/components/Question.jsx
@@ -1,19 +1,21 @@
 import PropTypes from "prop-types";
 import Options from "./Options";
 
-const Question = ({ question }) => {
+const Question = ({ question, dispatch, answer }) => {
   console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
 
-      <Options question={question} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 };
 
 Question.propTypes = {
   question: PropTypes.object.isRequired,
+  dispatch: PropTypes.any.isRequired,
+  answer: PropTypes.number,
 };
 
 export default Question;
