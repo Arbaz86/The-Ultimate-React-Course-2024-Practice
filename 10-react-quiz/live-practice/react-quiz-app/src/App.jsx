30af072d1d69c80c88cd50374e834ae157015c5d
Arbaz Hussain <arbazh777@gmail.com>
2024-06-24 16:14:47 +0530

    add stasrtScreen and Main component and Handling Loading, Error, and Ready Status with ruseRducer

--- 10-react-quiz/live-practice/react-quiz-app/src/App.jsx
+++ 10-react-quiz/live-practice/react-quiz-app/src/App.jsx
@@ -1,11 +1,51 @@
-import reactLogo from "./assets/react.svg";
+import { useEffect, useReducer } from "react";
 import "./App.css";
-import DateCounter from "./components/DateCounter.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Loader from "./components/Loader.jsx";
+import Error from "./components/Error.jsx";
+import StartScreen from "./components/StartScreen.jsx";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case "dataReceived":
+      return { ...state, questions: payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
-      <DateCounter />
+      <Header />
+
+      <main className="main">
+        <Main>
+          {status === "loading" && <Loader />}
+          {status === "Error" && <Error />}
+          {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        </Main>
+      </main>
     </div>
   );
 }
