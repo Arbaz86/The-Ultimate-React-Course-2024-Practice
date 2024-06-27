59379bd1385e21a01e0dbee1df685898385a5047
Arbaz Hussain <arbazh777@gmail.com>
2024-06-27 16:43:59 +0530

    implement navigation with AppNav and PageNav component

--- 11-worldwise/live-practice/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/live-practice/worldwise/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import PageNav from "./PageNav";
+import PageNav from "../components/PageNav";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div>
       <PageNav />
-
       <h1>Worldwise</h1>
+
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 };
