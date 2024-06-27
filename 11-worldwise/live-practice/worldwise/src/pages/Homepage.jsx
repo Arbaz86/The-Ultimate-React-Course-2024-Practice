562c1b7a8364f1508399060559090651ed3a72ed
Arbaz Hussain <arbazh777@gmail.com>
2024-06-27 16:10:31 +0530

    Implementing Main Pages and Routes and Page not found

--- 11-worldwise/live-practice/worldwise/src/pages/Homepage.jsx
+++ 11-worldwise/live-practice/worldwise/src/pages/Homepage.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return <div>Worldwise</div>;
 };
 
 export default Homepage;
