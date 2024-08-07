236e4fbafb4cad72081b86a79904b8d2a5c5052f
Arbaz Hussain <arbazh777@gmail.com>
2024-08-07 16:01:18 +0530

    Implement useUrkPosition Hook to Retrieve Lat and Lng, and Fetch City Data in Form

--- 11-worldwise/live-practice/worldwise/src/components/Map.jsx
+++ 11-worldwise/live-practice/worldwise/src/components/Map.jsx
@@ -12,11 +12,11 @@ import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useGeolocation } from "../hooks/useGeolocation";
 import Button from "./Button";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 const Map = () => {
   const { cities } = useCities();
 
-  const [searchParams] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const {
     isLoading: isLoadingPosition,
@@ -24,8 +24,7 @@ const Map = () => {
     getPosition,
   } = useGeolocation();
 
-  const mapLat = searchParams.get("lat");
-  const mapLng = searchParams.get("lng");
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) {
